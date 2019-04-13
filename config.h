9f915c52eeeb826cd368a73546f8838acda7aaf0
LVTTL <contact@lvttl.xyz>
2019-04-13 12:58:16 +0200

    changed default terminal size to 120x48

--- config.h
+++ config.h
@@ -138,8 +138,8 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
-static unsigned int rows = 24;
+static unsigned int cols = 120;
+static unsigned int rows = 48;
 
 /*
  * Default shape of the mouse cursor
